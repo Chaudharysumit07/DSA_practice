9451d6b740c21df2f2f13ef4366767435e67b32e
Sumit Chaudhary <39550494+Chaudharysumit07@users.noreply.github.com>
2025-02-12 13:03:11 +0530

    Create Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.py

---
+++ leetcode_daily_problems/Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits.py
@@ -0,0 +1,63 @@
+# date 12 feb 2025
+# Question link : https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/description/
+
+# my solution :
+
+class Solution:
+    def sumOfDigits(self, num: int) -> int:
+        sod = 0
+        while num > 0:
+            sod += num % 10
+            num //= 10  # Fix: Integer division
+        return sod
+
+
+    def findTwoLargest(self, array,num):
+        myList=[]
+        if num > array[0] :
+            if array[0] >array[1]:
+                myList.append(num)
+                myList.append(array[0])
+            else:
+                myList.append(num)
+                myList.append(array[1])
+        else:
+            if num > array[1]:
+                myList.append(num)
+                myList.append(array[0])
+            else:
+                myList.append(array[1])
+                myList.append(array[0])
+        return myList
+
+    def maximumSum(self, nums: List[int]) -> int:
+        digSumDict = {}
+
+        # print("Processing input numbers...")
+        for num in nums:
+            sod = self.sumOfDigits(num)
+            # print(f"Number: {num}, Sum of Digits: {sod}")
+
+            if sod not in digSumDict:
+                digSumDict[sod] = [num]
+            else:
+                if len(digSumDict[sod]) < 2:
+                    digSumDict[sod].append(num)  # Fix: Use append()
+                else:
+                    twonumarray = self.findTwoLargest(digSumDict[sod],num)
+                    # print(f"twonumarray: {twonumarray}")
+                    digSumDict[sod] = twonumarray
+        max_sum = -1
+        # print("\nChecking maximum sum pairs...")
+
+        for arrays in digSumDict.values():
+            # print(f"arrays: {arrays}")
+            if len(arrays) >1:
+                sumtot=0
+                for num in arrays:
+                    sumtot+=num
+                # print(f"Sumtotal: {sumtot}")
+                max_sum=max(sumtot,max_sum)
+
+        # print(f"Maximum sum of any two numbers with the same digit sum: {max_sum}\n")
+        return max_sum
